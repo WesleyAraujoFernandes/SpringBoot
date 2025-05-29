d54d4d7dc30afd9b18a48a0891274aef3ca473b4
Wesley Fernandes <wesley@viaapia.com.br>
2025-05-29 18:00:48 -0300

    Iniciando o hateoas

--- src/main/java/com/example/springboot/models/ProductModel.java
+++ src/main/java/com/example/springboot/models/ProductModel.java
@@ -4,17 +4,21 @@ import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.UUID;
 
+import org.springframework.hateoas.RepresentationModel;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Entity
 @Table(name = "TB_PRODUCTS")
 @Data
-public class ProductModel implements Serializable{
+@EqualsAndHashCode(callSuper=false)
+public class ProductModel extends RepresentationModel<ProductModel> implements Serializable{
     private static final long serialVersionUID = 1L;
 
     @Id
